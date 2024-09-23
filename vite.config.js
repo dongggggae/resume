ea4545c7ea089a75f98f24a36c4d671f0b3458b2
김동현 <doyn0822@hiconsy.com>
2024-09-23 15:31:18 +0900

    Chore : 기본 설정 수정 및 추가

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/resume/',
+  css: {
+    devSourcemap: true,
+  },
+  // resolve: {
+  //   alias: {
+  //     '@': '/src'
+  //   }
+  // },
   plugins: [react()],
 })
