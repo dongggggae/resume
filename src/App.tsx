04320cc08b55a30482d841e59c1294fbc9f43771
김동현 <doyn0822@hiconsy.com>
2024-11-05 16:02:46 +0900

    임시 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { Heading } from 'hyun-design-system';
+
+import './assets/scss/style.scss';
+import Header from './components/Header';
+import SectionTitle from './components/SectionTitle';
+
 function App() {
-  return <div className="container"></div>;
+  return (
+    <>
+      <Header />
+      <section>
+        <div className="container w-container">
+          <Heading>Section</Heading>
+        </div>
+      </section>
+      <section>
+        <div className="container w-container">
+          <SectionTitle />
+          {/* <Text size="xl">asdasd</Text> */}
+        </div>
+      </section>
+      <section>
+        <div className="container w-container">
+          <Heading>Section</Heading>
+        </div>
+      </section>
+      <section>
+        <div className="container w-container">
+          <Heading>Section</Heading>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
