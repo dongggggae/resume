a0988272e2dd1522d046beb4a3244d5d85941034
hyun <wjdqls149@naver.com>
2025-01-15 18:44:12 +0900

    포트번호 설정, 소스맵 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+  },
   base: '/resume/',
+  css: {
+    devSourcemap: true,
+  },
 });
