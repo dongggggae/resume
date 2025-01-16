07f462b3bdb6fe9d815bfae25719f252c8674c3e
hyun <wjdqls149@naver.com>
2025-01-16 21:13:14 +0900

    기존 파일 수정

--- src/components/common/StepTitle.tsx
+++ src/components/common/StepTitle.tsx
@@ -4,9 +4,10 @@ import { Heading } from 'hyun-design-system';
 
 const StepTitle = ({ title }) => {
   return (
-    <div className="step-title mt-8 mb-8">
+    <div className="step-title mt-10 mb-8">
       <Heading size={3} level="h3" color="primary" className="fw--600">
         {title}
+        <span className='fc--information'>.</span>
       </Heading>
     </div>
   );
