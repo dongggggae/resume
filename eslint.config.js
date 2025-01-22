20b3f89cdaf2a6f91cbee343f870b732a8f647c9
hyun <wjdqls149@naver.com>
2025-01-22 12:29:06 +0900

    lint 설정, ts 설정 규칙 변경

--- eslint.config.js
+++ eslint.config.js
@@ -5,19 +5,17 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: typescriptParser,
+      ecmaVersion: 'latest',
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      sourceType: 'module',
     },
     settings: { react: { version: '18.3' } },
     plugins: {
@@ -29,6 +27,7 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...typescript.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
